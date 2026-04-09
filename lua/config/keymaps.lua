b0ec342e7772d9f636fa2c3d60f1fc2e290c6482
Enze Chi <Enze.Chi@gmail.com>
2026-04-09 15:47:35 +1000

    add keybinding for gitsigns

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>x", ":wq<CR>", { desc = "Save and Quit" })
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Search" })
+-- vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Search" })
 vim.keymap.set("n", "<leader>s", ":split<CR>", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Vertical Split" })
 vim.keymap.set("n", "<leader>tn", ":set number!<CR>", { desc = "Toggle number" })
@@ -20,3 +20,11 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- gitsigns
+vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
+vim.keymap.set("n", "<leader>hb", "<cmd>Gitsigns blame_line<CR>", { desc = "Blame line" })
+vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
+vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+vim.keymap.set("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "Next hunk" })
+vim.keymap.set("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Previous hunk" })
