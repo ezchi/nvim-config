a7e15a65498b9f7fb1466b6b55f0da0fcd00a833
Enze Chi <Enze.Chi@gmail.com>
2026-04-11 16:17:52 +1000

    add nvim-cmp and keybinding for switch between windows and terminals

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,18 @@ vim.keymap.set("n", "<leader>x", ":wq<CR>", { desc = "Save and Quit" })
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Search" })
 vim.keymap.set("n", "<leader>s", ":split<CR>", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Vertical Split" })
+
+-- Window Navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+
+-- Terminal Navigation
+vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Go to Left Window" })
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Go to Lower Window" })
+vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Go to Upper Window" })
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Go to Right Window" })
 vim.keymap.set("n", "<leader>tn", ":set number!<CR>", { desc = "Toggle number" })
 vim.keymap.set("n", "<leader>tr", ":set relativenumber!<CR>", { desc = "Toggle relative number" })
 
@@ -21,4 +33,10 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-
+-- AI
+vim.keymap.set("n", "<leader>ag", function()
+    vim.cmd("botright vsplit")
+    local width = math.floor(vim.o.columns / 3)
+    vim.cmd("vertical resize " .. width)
+    vim.cmd("terminal gemini")
+end, { desc = "Open Gemini CLI" })
