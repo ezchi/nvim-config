17bf48f775b68be0bfedeba40ae9bc2581fe703c
Enze Chi <Enze.Chi@gmail.com>
2026-04-11 19:22:38 +1000

    fix the window move keybinding conflicts with tmux issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,11 +9,6 @@ vim.keymap.set("n", "<leader>s", ":split<CR>", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Vertical Split" })
 
 -- Window Navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
-
 -- Terminal Navigation
 vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Go to Left Window" })
 vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Go to Lower Window" })
